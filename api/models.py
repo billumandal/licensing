7a6352b620fe52f6caaee8f8b68e24798574460b
billumandal <billu.mandal@gmail.com>
2017-03-16 17:52:33 +0530

    more commissions

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    ip_addr = models.GenericIPAddressField(protocol='both')
--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,11 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'reference', 'hash')
+        fields = ('url', 'username', 'email', 'groups')
+        # fields = ('url', 'username', 'reference', 'hash')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
+        fields = ('url', 'name')
         fields = ('url', 'name', 'files')
